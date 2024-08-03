7bfe258b947570091c11d28460a60f2485b96ac5
KTH1007 <rlaxogjs2684@gmail.com>
2024-08-04 00:33:29 +0900

    Feat: security 설정

---
+++ src/main/java/me/tae/tutorial/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package me.tae.tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/hello"
+                        ).permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .cors(cors -> cors.disable())
+                .build();
+    }
+}
