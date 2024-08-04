acc5c5e5b0dc33d44c0392a6f816e996541c939c
KTH1007 <rlaxogjs2684@gmail.com>
2024-08-04 17:53:33 +0900

    Feat: H2 DB 접근 권한 설정

--- src/main/java/me/tae/tutorial/config/SecurityConfig.java
+++ src/main/java/me/tae/tutorial/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,13 +14,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/api/hello"
+                        .requestMatchers("/api/hello",
+                                "/h2-console/**"
                         ).permitAll()
                         .anyRequest()
                         .authenticated()
                 )
-                .cors(cors -> cors.disable())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 // enable h2-console
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
