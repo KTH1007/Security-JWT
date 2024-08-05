a32705f9767b15bee275cf4c3fdcc4c79325c275
KTH1007 <rlaxogjs2684@gmail.com>
2024-08-05 10:25:41 +0900

    Refactor: import 수정

--- src/main/java/me/tae/tutorial/config/SecurityConfig.java
+++ src/main/java/me/tae/tutorial/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package me.tae.tutorial.config;
 
 import lombok.RequiredArgsConstructor;
-import me.tae.tutorial.jwt.*;
+import me.tae.tutorial.jwt.JwtAccessDeniedHandler;
+import me.tae.tutorial.jwt.JwtAuthenticationEntryPoint;
+import me.tae.tutorial.jwt.JwtFilter;
+import me.tae.tutorial.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Configuration
